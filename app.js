965278fc186738ddb578ab8d29fb9a5ea5dafb83
yonasbek <yonasbek4@gmail.com>
2023-10-17 09:04:11 +0300

    * Update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose");
 const url = 'mongodb://127.0.0.1:27017/EMS'
+// this is a test push
 const app = express()
 require('dotenv').config();
 
